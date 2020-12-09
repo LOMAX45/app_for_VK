aeaf138a7d058462f3df7afacf7334b98bc2f0f2
Maxim LOSEV <losev-maksim@mail.ru>
2020-12-09 20:11:04 +0300

    lesson2 code review

--- LMapp_for_VK/LMapp_for_VK/AppTableViewController.swift
+++ LMapp_for_VK/LMapp_for_VK/AppTableViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 class AppTableViewController: UITableViewController {
 
     let friends = ["Самый лучший друг", "Лучший друг", "Не самый лучший друг", "Друг"]
-    
     let groups = ["Группа 1", "Группа 2", "Группа 3", "Группа 4"]
     
     @IBOutlet var friendsTableView: UITableView!
@@ -20,21 +19,7 @@ class AppTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
-    // MARK: - Table view data source
-    
-    /*
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
- */
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch tableView {
@@ -58,8 +43,8 @@ class AppTableViewController: UITableViewController {
             cell.textLabel?.text = groups[indexPath.row]
             return cell
         default:
-            let brake = tableForBreak.dequeueReusableCell(withIdentifier: "break")
-            return brake!
+            let cell = tableView.visibleCells[0]
+            return cell
         }
     }
     
@@ -71,52 +56,5 @@ class AppTableViewController: UITableViewController {
             groupsTableView.deselectRow(at: indexPath, animated: true)
         default: break
         }
-        
     }
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- LMapp_for_VK/LMapp_for_VK/LoginFormController.swift
+++ LMapp_for_VK/LMapp_for_VK/LoginFormController.swift
@@ -1,5 +1,5 @@
 //
-//  LoginFormControllerViewController.swift
+//  LoginFormController.swift
 //  LMapp_for_VK
 //
 //  Created by Максим Лосев on 08.12.2020.
@@ -12,80 +12,70 @@ class LoginFormController: UIViewController {
     @IBOutlet weak var nickNameEntInput: UITextField!
     @IBOutlet weak var passwordEntInput: UITextField!
     @IBOutlet weak var scrollingEnt: UIScrollView!
-    @IBOutlet weak var nickNameregInput: UITextField!
+    
+    @IBOutlet weak var nickNameRegInput: UITextField!
     @IBOutlet weak var passwordRegInput: UITextField!
     @IBOutlet weak var nameRegInput: UITextField!
     @IBOutlet weak var familyNameRegInput: UITextField!
     @IBOutlet weak var scrollingReg: UIScrollView!
     
-    var listOfUsers = [["admin", "admin", "admin", "admin"], ["testuser", "12345", "Test", "User"]]
+    var listOfUsers = [
+        ["admin", "admin", "admin", "admin"],
+        ["testuser", "12345", "Test", "User"]
+    ]
     
     @IBAction func unwindSegue(sender: UIStoryboardSegue) {}
-    
     @IBAction func regButtonPressed(_ sender: Any) {}
-    
     @IBAction func enterButtonPressed(_ sender: Any) {}
-    
-    @IBAction func RegProcess(_ sender: Any) {}
     
     private func alerting (title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-                    // Создаем кнопку для UIAlertController
+        // Создаем кнопку для UIAlertController
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
-                    // Добавляем кнопку на UIAlertController
-                    alert.addAction(action)
-                    // Показываем UIAlertController
-                    present(alert, animated: true, completion: nil)
+        // Добавляем кнопку на UIAlertController
+        alert.addAction(action)
+        // Показываем UIAlertController
+        present(alert, animated: true, completion: nil)
     }
     
-    private func checkInputsReg () -> Bool {
-        return (nickNameregInput.text != "" && passwordRegInput.text != "" && nameRegInput.text != "" && familyNameRegInput.text != "")
+    private func checkInputsFilling () -> Bool {
+        return (nickNameRegInput.text != "" && passwordRegInput.text != "" && nameRegInput.text != "" && familyNameRegInput.text != "")
     }
     
-    private func checkNickAvailability () -> Bool {
+    private func checkAvailabilityOfNick (nick: UITextField) -> Int {
         var i = 0
-        while i < listOfUsers.count && nickNameregInput.text != listOfUsers[i][0] {
+        while i < listOfUsers.count && nick.text != listOfUsers[i][0] {
             i += 1
         }
-        if i < listOfUsers.count {
-            return false
-        } else {
-            return true
-        }
-    }
-    
-    private func checkLogOnCredentials () -> Bool {
-        var i = 0
-        while i < listOfUsers.count && nickNameEntInput.text != listOfUsers[i][0] {
-            i += 1
-        }
-        if i < listOfUsers.count {
-            return passwordEntInput.text == listOfUsers[i][1]
-        } else {
-            return false
-        }
+        return i
     }
     
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
         
         switch identifier {
         case "Registration":
-            if !checkNickAvailability() {
-                alerting(title: "Ошибка", message: "Пользователь с таким ником уже существует")
+            if !checkInputsFilling() {
+                alerting(title: "ОШИБКА", message: "Все поля должны быть заполнены")
                 return false
-            } else if checkInputsReg() {
-                listOfUsers.append([nickNameregInput.text!, passwordRegInput.text!, nameRegInput.text!, familyNameRegInput.text!])
+            } else if checkAvailabilityOfNick(nick: nickNameRegInput) < listOfUsers.count {
+                alerting(title: "ОШИБКА", message: "Пользователь с таким ником уже существует.")
+                return false
+            } else {
+                listOfUsers.append([nickNameRegInput.text!, passwordRegInput.text!, nameRegInput.text!, familyNameRegInput.text!])
                 print(listOfUsers)
                 return true
-            } else {
-                alerting(title: "Ошибка", message: "Все поля должны быть заполнены")
-                return false
             }
         case "LogOn":
-            if checkLogOnCredentials() {
-                return true
+            if checkAvailabilityOfNick(nick: nickNameEntInput) < listOfUsers.count {
+                let checkPass = passwordEntInput.text == listOfUsers[checkAvailabilityOfNick(nick: nickNameEntInput)][1]
+                if checkPass == false {
+                    alerting(title: "ОШИБКА", message: "Вы ввели неверный пароль")
+                    return false
+                } else {
+                    return true
+                }
             } else {
-                alerting(title: "Ошибка", message: "Неверный логин или пароль.")
+                alerting(title: "ОШИБКА", message: "Пользователя с таким ником не существует")
                 return false
             }
         default:
@@ -93,93 +83,53 @@ class LoginFormController: UIViewController {
         }
     }
     
-
-    
-    
     // Когда клавиатура появляется на экране входа
-    
-        @objc func keyboardWasShownEnt(notification: Notification) {
-            
-            // Получаем размер клавиатуры
-            let info = notification.userInfo! as NSDictionary
-            let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
-            let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
-            
-            // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
-            self.scrollingEnt?.contentInset = contentInsets
-            scrollingEnt?.scrollIndicatorInsets = contentInsets
-        }
-    
-    // Когда клавиатура появляется на экране регистрации
-        @objc func keyboardWasShownReg(notification: Notification) {
-            
-            // Получаем размер клавиатуры
-            let info = notification.userInfo! as NSDictionary
-            let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
-            let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
-            
-            // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
-            self.scrollingReg?.contentInset = contentInsets
-            scrollingReg?.scrollIndicatorInsets = contentInsets
-        }
-
+    @objc func keyboardWasShown(notification: Notification) {
         
-        //Когда клавиатура исчезает на экране входа
-        @objc func keyboardWillBeHiddenEnt(notification: Notification) {
-            // Устанавливаем отступ внизу UIScrollView, равный 0
-            let contentInsets = UIEdgeInsets.zero
-            scrollingEnt?.contentInset = contentInsets
-        }
-    
-    //Когда клавиатура исчезает на экране регистрации
-    @objc func keyboardWillBeHiddenReg(notification: Notification) {
+        // Получаем размер клавиатуры
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+        
+        // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
+        self.scrollingEnt?.contentInset = contentInsets
+        scrollingEnt?.scrollIndicatorInsets = contentInsets
+        
+        self.scrollingReg?.contentInset = contentInsets
+        scrollingReg?.scrollIndicatorInsets = contentInsets
+    }
+        
+    //Когда клавиатура исчезает на экране входа
+    @objc func keyboardWillBeHidden(notification: Notification) {
         // Устанавливаем отступ внизу UIScrollView, равный 0
         let contentInsets = UIEdgeInsets.zero
+        scrollingEnt?.contentInset = contentInsets
         scrollingReg?.contentInset = contentInsets
     }
     
-    @objc func hideKeyboardEnt() {
-            self.scrollingEnt?.endEditing(true)
-        }
-    
-    @objc func hideKeyboardReg() {
-            self.scrollingReg?.endEditing(true)
-        }
+    @objc func hideKeyboard() {
+        self.scrollingEnt?.endEditing(true)
+        self.scrollingReg?.endEditing(true)
+    }
 
-
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Подписываемся на два уведомления: одно приходит при появлении клавиатуры
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShownEnt), name: UIResponder.keyboardWillShowNotification, object: nil)
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShownReg), name: UIResponder.keyboardWillShowNotification, object: nil)
-        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
         // Второе — когда она пропадает
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHiddenEnt(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHiddenReg(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         // Жест нажатия
-        let hideKeyboardGestureEnt = UITapGestureRecognizer(target: self, action: #selector(hideKeyboardEnt))
-        
-        let hideKeyboardGestureReg = UITapGestureRecognizer(target: self, action: #selector(hideKeyboardReg))
-        
+        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         // Присваиваем его UIScrollVIew
-        scrollingEnt?.addGestureRecognizer(hideKeyboardGestureEnt)
-        
-        scrollingReg?.addGestureRecognizer(hideKeyboardGestureReg)
-
-        // Do any additional setup after loading the view.
+        scrollingEnt?.addGestureRecognizer(hideKeyboardGesture)
+        scrollingReg?.addGestureRecognizer(hideKeyboardGesture)
     }
-    
     
     override func viewWillDisappear(_ animated: Bool) {
             super.viewWillDisappear(animated)
@@ -187,17 +137,4 @@ class LoginFormController: UIViewController {
             NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
             NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
         }
-    
-    /*
-     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
-
-
